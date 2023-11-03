6fc81bc6f5c54111e9ee638fbcbef0fabeac11a6
sultanasafia <147336363+sultanasafia@users.noreply.github.com>
2023-11-03 17:22:37 +0500

    js-ass-6

---
+++ app.js
@@ -0,0 +1,50 @@
+// Q1===============
+// let a = 10;
+// document.write("The value of  is :" + a)
+// document.write('<br>')
+// ++a
+// document.write("The value of ++a is :" + a)
+// document.write('<br>')
+// document.write("Now The value of a is :" + a)
+// document.write('<br>')
+// a++
+// document.write("Now The value of a++ is :" + a)
+// document.write('<br>')
+// --a
+// document.write("Now The value of --a is :" + a)
+// document.write('<br>')
+// a--
+// document.write("Now The value of a-- is :" + a)
+
+// Q2============
+
+// var a = 2, b = 1;
+// var ans = --a - --b + ++b + b--;
+// --a;
+// --a - --b;
+// --a - --b + ++b;
+// --a - --b + ++b + b--
+// document.write(a)
+// document.write(b)
+// Q3===================
+// let user = prompt('Enter name')
+// document.write("Hi! "+ user)
+
+// Q5=================
+// let table = +prompt("Enter your table");
+// for(let i = 1 ; i<= 10; i++ ){
+//     document.write(table + " x " + i +" = "+ table*i+ "<br>" )
+// }
+
+// Q6=============
+// let totalMarks = 100;
+// let engMarks = 54;
+// let mathsMarks = 54;
+// let urduMarks = 48;
+// let sum = engMarks+ mathsMarks + urduMarks/100 *100
+// document.write("english percentage"+engMarks/100*100)
+// document.write("maths percentage"+mathsMarks/100*100)
+// document.write("urdu percentage"+urduMarks/100*100)
+
+// document.write(sum)
+// COMPLETED TASK===================
